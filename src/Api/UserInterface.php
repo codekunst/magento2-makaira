d5111a9194e59a42e3dfde525d8aade9e5dcdd89
Sandra <sandra@codekunst.com>
2021-02-04 14:39:01 +0100

    Add files and code for user GET Request

---
+++ src/Api/UserInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Makaira\Headless\Api;
+
+interface UserInterface
+{
+    /**
+     * GET Method
+     *
+     * @return string
+     * @api
+     */
+    public function get();
+
+}
