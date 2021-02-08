bddcee9ebc9e0e5076f6b5c92a1dc2a692fb6f0e
Sandra <sandra@codekunst.com>
2021-02-08 08:59:06 +0100

    Change User Return to success: true if user is not logged in

--- src/Model/User.php
+++ src/Model/User.php
@@ -42,7 +42,7 @@ class User implements UserInterface
             ];
         } else {
             $response = [
-                "success" => false,
+                "success" => true,
                 "message" => "Kunde ist nicht angemeldet."
             ];
         }
